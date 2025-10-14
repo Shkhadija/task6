7a0fe037721195c2a507232ced777749896c00b4
Shkhadija <xediceserif@gmail.com>
2025-10-14 07:12:36 +0400

    tapsiriq6

---
+++ tapsiriq6/Form1.Designer.cs
@@ -0,0 +1,426 @@
+﻿namespace tapsiriq6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox4 = new GroupBox();
+            button4 = new Button();
+            textBox8 = new TextBox();
+            textBox7 = new TextBox();
+            maskedTextBox4 = new MaskedTextBox();
+            label12 = new Label();
+            label11 = new Label();
+            label10 = new Label();
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            maskedTextBox1 = new MaskedTextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            groupBox2 = new GroupBox();
+            button2 = new Button();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            maskedTextBox2 = new MaskedTextBox();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            groupBox3 = new GroupBox();
+            button3 = new Button();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            maskedTextBox3 = new MaskedTextBox();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            groupBox4.SuspendLayout();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(button4);
+            groupBox4.Controls.Add(textBox8);
+            groupBox4.Controls.Add(textBox7);
+            groupBox4.Controls.Add(maskedTextBox4);
+            groupBox4.Controls.Add(label12);
+            groupBox4.Controls.Add(label11);
+            groupBox4.Controls.Add(label10);
+            groupBox4.Location = new Point(431, 219);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(357, 219);
+            groupBox4.TabIndex = 0;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "groupBox4";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(257, 157);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 20;
+            button4.Text = "Rezerv et";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(159, 128);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(125, 27);
+            textBox8.TabIndex = 16;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(159, 81);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(125, 27);
+            textBox7.TabIndex = 15;
+            // 
+            // maskedTextBox4
+            // 
+            maskedTextBox4.Location = new Point(159, 36);
+            maskedTextBox4.Mask = "00";
+            maskedTextBox4.Name = "maskedTextBox4";
+            maskedTextBox4.Size = new Size(125, 27);
+            maskedTextBox4.TabIndex = 8;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(6, 128);
+            label12.Name = "label12";
+            label12.Size = new Size(98, 20);
+            label12.TabIndex = 4;
+            label12.Text = "Qalan mudet:";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(6, 81);
+            label11.Name = "label11";
+            label11.Size = new Size(45, 20);
+            label11.TabIndex = 4;
+            label11.Text = "Qrup:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(6, 39);
+            label10.Name = "label10";
+            label10.Size = new Size(64, 20);
+            label10.TabIndex = 4;
+            label10.Text = "Muddet:";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(maskedTextBox1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(357, 201);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(257, 155);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 17;
+            button1.Text = "Rezerv et";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(125, 123);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(125, 78);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 9;
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(125, 32);
+            maskedTextBox1.Mask = "00";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(125, 27);
+            maskedTextBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(11, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Muddet:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(11, 78);
+            label2.Name = "label2";
+            label2.Size = new Size(45, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Qrup:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(11, 123);
+            label3.Name = "label3";
+            label3.Size = new Size(98, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Qalan mudet:";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(textBox4);
+            groupBox2.Controls.Add(textBox3);
+            groupBox2.Controls.Add(maskedTextBox2);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Location = new Point(431, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(357, 201);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(257, 155);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 18;
+            button2.Text = "Rezerv et";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(159, 120);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(159, 78);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 11;
+            // 
+            // maskedTextBox2
+            // 
+            maskedTextBox2.Location = new Point(159, 35);
+            maskedTextBox2.Mask = "00";
+            maskedTextBox2.Name = "maskedTextBox2";
+            maskedTextBox2.Size = new Size(125, 27);
+            maskedTextBox2.TabIndex = 6;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(6, 123);
+            label6.Name = "label6";
+            label6.Size = new Size(98, 20);
+            label6.TabIndex = 3;
+            label6.Text = "Qalan mudet:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(6, 78);
+            label5.Name = "label5";
+            label5.Size = new Size(45, 20);
+            label5.TabIndex = 3;
+            label5.Text = "Qrup:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 35);
+            label4.Name = "label4";
+            label4.Size = new Size(64, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Muddet:";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(button3);
+            groupBox3.Controls.Add(textBox6);
+            groupBox3.Controls.Add(textBox5);
+            groupBox3.Controls.Add(maskedTextBox3);
+            groupBox3.Controls.Add(label9);
+            groupBox3.Controls.Add(label8);
+            groupBox3.Controls.Add(label7);
+            groupBox3.Location = new Point(12, 219);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(357, 201);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "groupBox3";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(257, 157);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 19;
+            button3.Text = "Rezerv et";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(125, 128);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(125, 27);
+            textBox6.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(125, 81);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(125, 27);
+            textBox5.TabIndex = 13;
+            // 
+            // maskedTextBox3
+            // 
+            maskedTextBox3.Location = new Point(125, 36);
+            maskedTextBox3.Mask = "00";
+            maskedTextBox3.Name = "maskedTextBox3";
+            maskedTextBox3.Size = new Size(125, 27);
+            maskedTextBox3.TabIndex = 7;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(11, 128);
+            label9.Name = "label9";
+            label9.Size = new Size(98, 20);
+            label9.TabIndex = 3;
+            label9.Text = "Qalan mudet:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(11, 81);
+            label8.Name = "label8";
+            label8.Size = new Size(45, 20);
+            label8.TabIndex = 3;
+            label8.Text = "Qrup:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(11, 39);
+            label7.Name = "label7";
+            label7.Size = new Size(64, 20);
+            label7.TabIndex = 3;
+            label7.Text = "Muddet:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(groupBox4);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox4;
+        private MaskedTextBox maskedTextBox4;
+        private Label label12;
+        private Label label11;
+        private Label label10;
+        private GroupBox groupBox1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private MaskedTextBox maskedTextBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private GroupBox groupBox2;
+        private MaskedTextBox maskedTextBox2;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private GroupBox groupBox3;
+        private MaskedTextBox maskedTextBox3;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Button button4;
+        private TextBox textBox8;
+        private TextBox textBox7;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private Button button3;
+        private TextBox textBox6;
+        private TextBox textBox5;
+    }
+}
---
+++ tapsiriq6/Form1.cs
@@ -0,0 +1,73 @@
+﻿namespace tapsiriq6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string muddet = maskedTextBox1.Text;
+            string qrup = textBox1.Text;
+            string qalanMuddet = textBox2.Text;
+
+            if (string.IsNullOrWhiteSpace(muddet) || string.IsNullOrWhiteSpace(qrup))
+            {
+                MessageBox.Show("Zəhmət olmasa bütün xanaları doldurun!");
+                return;
+            }
+
+            MessageBox.Show($"Rezerv olundu!\nMüddət: {muddet}\nQrup: {qrup}\nQalan müddət: {qalanMuddet}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string muddet = maskedTextBox2.Text;
+            string qrup = textBox3.Text;
+            string qalanMuddet = textBox4.Text;
+
+            if (string.IsNullOrWhiteSpace(muddet) || string.IsNullOrWhiteSpace(qrup))
+            {
+                MessageBox.Show("Zəhmət olmasa bütün xanaları doldurun!");
+                return;
+            }
+
+            MessageBox.Show($"Rezerv olundu!\nMüddət: {muddet}\nQrup: {qrup}\nQalan müddət: {qalanMuddet}");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string muddet = maskedTextBox3.Text;
+            string qrup = textBox5.Text;
+            string qalanMuddet = textBox6.Text;
+
+            if (string.IsNullOrWhiteSpace(muddet) || string.IsNullOrWhiteSpace(qrup))
+            {
+                MessageBox.Show("Zəhmət olmasa bütün xanaları doldurun!");
+                return;
+            }
+
+            MessageBox.Show($"Rezerv olundu!\nMüddət: {muddet}\nQrup: {qrup}\nQalan müddət: {qalanMuddet}");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string muddet = maskedTextBox4.Text;
+            string qrup = textBox7.Text;
+            string qalanMuddet = textBox8.Text;
+
+            if (string.IsNullOrWhiteSpace(muddet) || string.IsNullOrWhiteSpace(qrup))
+            {
+                MessageBox.Show("Zəhmət olmasa bütün xanaları doldurun!");
+                return;
+            }
+
+            MessageBox.Show($"Rezerv olundu!\nMüddət: {muddet}\nQrup: {qrup}\nQalan müddət: {qalanMuddet}");
+        }
+
+    }
+}
